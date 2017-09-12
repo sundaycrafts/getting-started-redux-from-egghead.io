4946cca3d9ea7ca0ee0486f5225661e4ed6d77ab
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-12 15:59:10 +0900

    Chapter 6: Store Methods: getState(), dispatch(), and subscribe()

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import counter from './counter'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// creatStore()にリデューサーを渡す
+const store = createStore(counter)
+
+const render = () => {
+  document.body.innerText = store.getState()
+}
+
+// storeの変更を監視して実行されるコールバックを登録(ここではレンダー関数)
+store.subscribe(render)
+// 初期表示
+render()
+
+document.addEventListener('click', () => {
+  // dispatchでアクションタイプを渡してリデューサーを実行
+  store.dispatch({ type: 'INCREMENT' })
+})
+
 registerServiceWorker()
