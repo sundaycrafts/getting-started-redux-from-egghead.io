7048d1fc0c61326deb2d9ede4e80dbef5b9e370b
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-12 16:12:38 +0900

    Chapter 7: Redux: Implementing Store from Scratch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import counter from './counter'
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+const createStore = (reducer) => {
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({})
+
+  return { getState, dispatch, subscribe }
+}
 
 // creatStore()にリデューサーを渡す
 const store = createStore(counter)
