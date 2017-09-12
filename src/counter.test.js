4a578eb8e15dec264288eb6f8d8878e096fede1d
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-12 15:21:37 +0900

    Chapter 5-1: counter reducer with tests

---
+++ src/counter.test.js
@@ -0,0 +1,10 @@
+import counter from './counter'
+
+it('counter test', () => {
+  expect(counter(0, { type: 'INCREMENT' })).toEqual(1)
+  expect(counter(1, { type: 'INCREMENT' })).toEqual(2)
+  expect(counter(2, { type: 'DECREMENT' })).toEqual(1)
+  expect(counter(1, { type: 'DECREMENT' })).toEqual(0)
+  expect(counter(0, { type: 'SOMETHING_ELSE' })).toEqual(0)
+  expect(counter(undefined, {})).toEqual(0)
+})
