5fcb5595fba2718739480b4defd5eed09ef8981a
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-13 16:50:21 +0900

    Chapter 13 : Redux: Reducer Composition with Arrays

--- src/todos.js
+++ src/todos.js
@@ -1,24 +1,35 @@
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      }
+    default:
+      return state
+  }
+}
+
 export default (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        ...state,
+        todo(undefined, action)
       ]
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo
-        }
-
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      })
+      return state.map(t => todo(t, action))
     default:
       return state
   }
