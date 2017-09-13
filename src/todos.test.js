d2a9d90dcb2f8683af864dad63e59ef9da7d7c4e
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-13 16:34:59 +0900

    Chapter 12: Redux: Writing a Todo List Reducer (Toggling a Todo)

--- src/todos.test.js
+++ src/todos.test.js
@@ -22,3 +22,41 @@ it('add todo', () => {
 
   expect(todos(stateBefore, action)).toEqual(stateAfter)
 })
+
+it('toggle todo', () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ]
+
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  }
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ]
+
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter)
+})
