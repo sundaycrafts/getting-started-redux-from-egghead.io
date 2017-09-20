140b6fd5e23590fc543cbf8d1aa01c6503f914be
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-20 13:01:07 +0900

    Chapter 2: Redux: Supplying the Initial State

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 import { TodoApp, todoApp } from './todos'
 
+const persistedState = {
+  todos: [{
+    id: '0',
+    text: 'Welcome back!',
+    completed: false
+  }]
+}
+const store = createStore(todoApp, persistedState)
+
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('root')
