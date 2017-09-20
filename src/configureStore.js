1a1824ab0d456cbd072241f9fe1e5e1bfb2619ee
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-20 14:31:38 +0900

    Chapter 4: Redux: Refactoring the Entry Point

---
+++ src/configureStore.js
@@ -0,0 +1,16 @@
+import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+const configureStore = () => {
+  const persistedState = loadState()
+  const store = createStore(todoApp, persistedState)
+  store.subscribe(throttle(() => {
+    todos: saveState({todos: store.getState().todos})
+  }, 1000))
+
+  return store
+}
+export default configureStore
+
