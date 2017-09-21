24d381e5565a008c94027fccb1e272adbd6d1f1f
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-22 01:45:59 +0900

    Chapter 10: Redux: Colocating Selectors with Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,9 @@
-import todos from './todos'
+import todos, * as fromTodos from './todos'
 import visibilityFilter from './visibilityFilter'
 import { combineReducers } from 'redux'
-// const combineReducers = (reducers) =>
-//   (state = {}, action) => Object.keys(reducers).reduce(
-//     (nextState, key) => {
-//       nextState[key] = reducers[key](
-//         state[key],
-//         action
-//       )
-//       return nextState
-//     },
-//     {}
-//   )
 
 const todoApp = combineReducers({ todos, visibilityFilter })
 export default todoApp
+
+export const getVisibleTodos = (state, filter) =>
+  fromTodos.getVisibleTodos(state.todos, filter)
