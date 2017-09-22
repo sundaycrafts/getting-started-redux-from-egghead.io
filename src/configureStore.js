ce0bbbf9eac49cc6435e65f4c4c9f6162dc75086
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-22 14:26:42 +0900

    Chapter 16: Redux: Wrapping dispatch() to Recognize Promises

--- src/configureStore.js
+++ src/configureStore.js
@@ -16,12 +16,25 @@ const addLoggingToDispatch = (store) => {
   }
 }
 
+const addPromiseSupportToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  return (action) => {
+    if (typeof action.then === 'function') {
+      return action.then(rawDispatch)
+    } else {
+      return rawDispatch(action)
+    }
+  }
+}
+
 const configureStore = () => {
   const store = createStore(todoApp)
 
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store)
   }
+
+  store.dispatch = addPromiseSupportToDispatch(store)
 
   return store
 }
