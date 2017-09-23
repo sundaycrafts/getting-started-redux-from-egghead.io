a41eaa169dd99238e54db00c3b7cec8a5393ab66
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-09-23 19:55:50 +0900

    Chapter 23-1: Redux: Avoiding Race Conditions with Thunks

--- src/api/index.js
+++ src/api/index.js
@@ -22,8 +22,8 @@ const fakeDatabase = {
 const delay = (ms) =>
   new Promise(resolve => setTimeout(resolve, ms))
 
-export const fetchTodos = (filter) =>
-  delay(500).then(() => {
+export const fetchTodos = (filter, getState) =>
+  delay(5000).then(() => {
     switch (filter) {
       case 'all':
         return fakeDatabase.todos
