e2cb40d478824ac9e5f56cd02d72c3e613b366c0
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-01-16 18:08:36 +0900

    replace redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,19 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import todoApp from './reducers'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 const configureStore = () => {
   const middlewares = [thunk]
+  const composeEnhancers = process.env.NODE_ENV === 'development' ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose :
+    compose
 
-  let composedMiddlewares
-  if (process.env.NODE_ENV === 'development') {
-    composedMiddlewares = composeWithDevTools(applyMiddleware(...middlewares))
-  } else {
-    composedMiddlewares = applyMiddleware(...middlewares)
-  }
-
-  const store = createStore(todoApp, composedMiddlewares)
+  const store = createStore(todoApp,
+    composeEnhancers(applyMiddleware(...middlewares)))
 
   return store
 }
